0bcfc9dcfe6e37f380aef4f62c10e423bfec2452
mdrabbi66 <mdnaimurrahmanrabbi6622@gmail.com>
2024-09-13 22:32:35 +0600

    update

--- plugins/commands.py
+++ plugins/commands.py
@@ -31,7 +31,7 @@ user_commands = [
     "me",
 ]
 avl_web = [
-    "droplink.co",
+    "bdshortner.com",
     "gplinks.in",
     "tnlink.in",
     "za.gl",
@@ -327,7 +327,7 @@ async def base_site_handler(bot, m: Message):
     user = await get_user(user_id)
     cmd = m.command
     site = user["base_site"]
-    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to droplink.co"
+    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to bdshortner.com"
     if len(cmd) == 1:
         return await m.reply(text=text, disable_web_page_preview=True)
     elif len(cmd) == 2:
--- translation.py
+++ translation.py
@@ -160,7 +160,7 @@ SHORTENER_API_MESSAGE = """To add or update your Shortner Website API,
             
 Ex: `/shortener_api 6LZq851sXofffPHugiKQq`
 
-Current Website: {base_site}
+Go to https://bdshortlink.com/member/tools/api
 
 
 Current Shortener API: `{shortener_api}`"""
